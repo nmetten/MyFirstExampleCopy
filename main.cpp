99de7bcde4a758daa1d680a49cda7b4b10a3ca39
Nikolas Metten <nmetten@csuchico.edu>
2026-09-02 11:48:32 -0700

    consistent with main branch

--- main.cpp
+++ main.cpp
@@ -12,8 +12,12 @@ int main()
     std::cout << "Addition: " << x + y << std::endl;
     std::cout << "Subtraction: " << x - y << std::endl;
     std::cout << "Multiplication: " << x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " << x % y << std::endl;
+    if (y == 0) {
+	    std::cout << "Invalid, divide by 0" << std::end;
+    } else {
+    	std::cout << "Division: " << x / y << std::endl;
+    	std::cout << "Remainder: " << x % y << std::endl;
+    }
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
 
